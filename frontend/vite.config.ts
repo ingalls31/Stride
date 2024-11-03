760a863fc91446fce65b42d23f15b0156721451f
duchuys31 <huy52670@gmail.com>
2024-11-03 16:00:41 +0700

    feat: move ssl folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
-import fs from 'fs';
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
   server: {
     https: {
-      key: fs.readFileSync('../ssl/key.pem'),
-      cert: fs.readFileSync('../ssl/cert.pem')
+      key: fs.readFileSync('ssl/key.pem'),
+      cert: fs.readFileSync('ssl/cert.pem')
     },
     port: 3000
   },
@@ -21,5 +21,5 @@ export default defineConfig({
     alias: {
       '~': path.resolve(__dirname, './src')
     }
-  },
+  }
 })
